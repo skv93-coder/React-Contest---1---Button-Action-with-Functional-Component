82bce42b95f194ea9e8b5c337c26435cdc8c0954
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-10-21 20:16:19 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [para, setPara] = useState("");
+  let sig=null;
+  const handleClick=()=>{
+    setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+    sig=9;
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={() => handleClick()}></button>
+      {sig?<p id='para'>{para}</p>}
     </div>
   );
 }
-
 
 export default App;
