ad975f7378815d2824765d6f7115571368dae3b1
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-10-21 20:44:06 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,17 @@ import "./../styles/App.css";
 
 function App() {
   const [para, setPara] = useState("");
-  let sig=null;
-  const handleClick=()=>{
-    setPara("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
-    sig=9;
-  }
+  let sig = null;
+  const handleClick = () => {
+    setPara(
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    );
+    sig = 9;
+  };
   return (
     <div id="main">
       <button id="click" onClick={() => handleClick()}></button>
-      {sig?<p id='para'>{para}</p>}
+      {sig ? <p id="para">{para}</p> : null}
     </div>
   );
 }
